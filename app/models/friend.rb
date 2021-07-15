e6e3aa53212034e87f2f3749b2e6d5a4f026a7fe
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-07-15 16:56:58 +0100

    update bootstrap styling

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,7 @@
 class Friend < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presence: true
+
 end
