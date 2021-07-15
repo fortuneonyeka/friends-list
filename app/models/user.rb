5a8aa1138a5a9e0d7d0e8d0c70336218badcf63d
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-07-15 21:57:24 +0100

    add basic bootstrap styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password_confirmation, presence: true
 
